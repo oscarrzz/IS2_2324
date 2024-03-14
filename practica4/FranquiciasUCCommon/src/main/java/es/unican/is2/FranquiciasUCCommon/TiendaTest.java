928aabd8c1598e416876f43dc38d0f80f59fe39c
CDiego <dcr909@alumnos.unican.es>
2024-03-14 12:30:44 +0100

    Tienda test creada

---
+++ practica4/FranquiciasUCCommon/src/main/java/es/unican/is2/FranquiciasUCCommon/TiendaTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.FranquiciasUCCommon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TiendaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
