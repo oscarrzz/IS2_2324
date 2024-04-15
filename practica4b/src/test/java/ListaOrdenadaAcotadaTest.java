4b122a9aed094a72f4b0b3f248293593ac6a81c9
“dcr909” <dcr909@alumnos.unican.es>
2024-04-15 17:30:52 +0200

    Añadido test remove y get

--- practica4b/src/test/java/ListaOrdenadaAcotadaTest.java
+++ practica4b/src/test/java/ListaOrdenadaAcotadaTest.java
@@ -1,12 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import es.unican.is2.listaOrdenadaAcotada.ListaOrdenadaAcotada;
 
 class ListaOrdenadaAcotadaTest {
-
-	ListaOrdenadaAcotada<Integer> lista = new ListaOrdenadaAcotada<Integer>(5);
+	
+	ListaOrdenadaAcotada<Integer> lista;
+	
+	@BeforeEach
+	public void testCrearLista() {
+		lista = new ListaOrdenadaAcotada<Integer>(5);
+	}
 	
 	@Test
 	public void testAdd() {
@@ -18,6 +24,22 @@ class ListaOrdenadaAcotadaTest {
 		lista.add(3);
 		lista.add(3);
 		assertThrows(IllegalStateException.class, () -> lista.add(4));
+	}
+	@Test
+	void testget() {
+		lista.add(3);
+		assertEquals(lista.get(0), 3);
+		assertThrows(IndexOutOfBoundsException.class, () -> lista.get(-5));
+		assertThrows(IndexOutOfBoundsException.class, () -> lista.get(21));
+		assertThrows(NullPointerException.class, () -> lista.get(1));
+	}
+	
+	@Test
+	void testRemove() {
+		lista.add(5);
+		assertEquals(lista.remove(0), 5);
+		assertThrows(IndexOutOfBoundsException.class, () -> lista.remove(6));
+		assertThrows(IndexOutOfBoundsException.class, () -> lista.remove(-1));
 	}
 	
 
